4839d27504bd9aadb1597186f13858a992c7b83a
Joselito Ramirez <114718947+empresario17@users.noreply.github.com>
2023-06-05 20:35:36 -0400

    entregable3_todoApi

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const db = new Sequelize({
-host: "localhost",
-port: 5432,
-database: "entregable3_api",
-username: "postgres",
-password: "choco2217",
-dialect: "postgres"
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+database: process.env.DB_NAME,
+username: process.env.DB_USERNAME,
+password: process.env.DB_PASSWORD,
+dialect: "postgres",
+dialectOptions: { ssl: { require: true, rejectUnauthorized: false }},
 });
 
 module.exports = db;
