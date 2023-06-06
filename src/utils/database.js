472bb92c472fd63949a0575fa5eb0163bac5114e
Joselito Ramirez <114718947+empresario17@users.noreply.github.com>
2023-06-05 20:52:35 -0400

    entregable3_todoApi

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,7 +8,7 @@ database: process.env.DB_NAME,
 username: process.env.DB_USERNAME,
 password: process.env.DB_PASSWORD,
 dialect: "postgres",
-dialectOptions: { ssl: { require: true, rejectUnauthorized: false }},
+//dialectOptions: { ssl: { require: true, rejectUnauthorized: false }},
 });
 
 module.exports = db;
